175a3da3447df131f4e18131f762d010a942a489
Joe LoPresti <jlopresti87@gmail.com>
2019-01-02 20:38:13 -0500

    Configure header responses

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+const parseHeaders = (req, res) => {
+    let ip = req.ip;
+    let language = req.headers['accept-language'];
+    const source = req.headers['user-agent'];
+
+    res.json({ip, language, source});
+
+  }
+
+app.use("/", express.static("./public"));
+
+app.get('/api/whoami', parseHeaders);
+   
+app.listen(PORT, () => {
+    console.log(`Timestamp Microservice is running on port ${PORT}...`);
+});
